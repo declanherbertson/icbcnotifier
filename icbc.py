64e6eb5ea9f1ce366ebec61ccd0b4d0fc8327b2d
declanherbertson <declanherbertson@gmail.com>
2021-08-11 17:33:13 -0700

    break out api logic

--- icbc.py
+++ icbc.py
@@ -1,42 +1,10 @@
 # TODO send SMS when increase in spots available
-import requests
 import myTime, constants, private
+from api import get_bearer_token, get_available_appointments
 from myEmail import Email
 import time
 from logger import Logger
 import json
-
-APPOINTMENTS_ENDPOINT = "https://onlinebusiness.icbc.com/deas-api/v1/web/getAvailableAppointments"
-LOGIN_ENDPOINT = "https://onlinebusiness.icbc.com/deas-api/v1/webLogin/webLogin"
-
-LOGIN_JSON = {
-  "drvrLastName": private.drvrLastName,
-  "licenceNumber": private.licenceNumber,
-  "keyword": private.keyword
-}
-
-def get_appointment_json():
-  return {
-    "aPosID": constants.aPosID,
-    "examType": constants.examType,
-    "examDate": myTime.ymd_format(myTime.tomorrow()),
-    "ignoreReserveTime": constants.ignoreReserveTime,
-    "prfDaysOfWeek": constants.prfDaysOfWeek,
-    "prfPartsOfDay": constants.prfPartsOfDay,
-    "lastName": private.lastName,
-    "licenseNumber": private.licenceNumber
-  }
-
-def get_available_appointments(bearer_token):
-  r = requests.post(APPOINTMENTS_ENDPOINT, json=get_appointment_json(), headers={
-    "Authorization": bearer_token
-  })
-  return r.status_code, r.reason, r.text
-
-def get_bearer_token():
-  r = requests.put(LOGIN_ENDPOINT, json=LOGIN_JSON)
-  bearer_token = r.headers["Authorization"]
-  return bearer_token
 
 def main():
   overview_logger = Logger(myTime.ymd_format(myTime.now()))
